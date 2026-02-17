04e98175aa71edbf04c0cdc6d3e9503a78e24a49
Kevin Santamaria <kevinsdasere@gmail.com>
2026-02-17 00:24:47 -0500

    update: update index components styles

--- src/layouts/components/Navbar.tsx
+++ src/layouts/components/Navbar.tsx
@@ -36,7 +36,7 @@ export const Navbar = () => {
   const navLinks = [
     { href: '#products', label: 'Productos', id: 'products' },
     { href: '#about', label: 'Nuestra Historia', id: 'about' },
-    { href: '#features', label: 'Servicios', id: 'features' },
+    { href: '#features', label: 'Esencia', id: 'features' },
     { href: '#where-to-buy', label: 'Encuéntranos', id: 'where-to-buy' },
   ]
 
@@ -88,7 +88,42 @@ export const Navbar = () => {
         className="text-foreground bg-primary/10 hover:bg-primary/25 rounded-full p-2 transition-colors md:hidden"
         onClick={() => setIsOpen(!isOpen)}
       >
-        {isOpen ? 'X' : '='}
+        {isOpen ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="icon icon-tabler icons-tabler-outline icon-tabler-x"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M18 6l-12 12"></path>
+            <path d="M6 6l12 12"></path>
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="icon icon-tabler icons-tabler-outline icon-tabler-menu-2"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M4 6l16 0"></path>
+            <path d="M4 12l16 0"></path>
+            <path d="M4 18l16 0"></path>
+          </svg>
+        )}
       </button>
 
       {/* Mobile Nav Menu */}
